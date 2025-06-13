121487763c271bec5860dea7339849024ca2d3a3
Araksha Puri <arakshapuri22@gmail.com>
2025-06-13 18:24:35 +0545

    [ADD] alembic to assets service

--- assets/database.py
+++ assets/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine=create_engine('postgresql://postgres:password@localhost:5433/test')
+engine=create_engine('postgresql://postgres:Puri%40222@localhost:5433/assets')
 SessionLocal=sessionmaker(bind=engine)
 Base=declarative_base()
 def get_db():
